e6c3d19039e9a91be84f184ee3c614c66f958dd7
Patrick Perey <the4dpatrick@yahoo.com>
2017-10-01 07:19:51 -0700

    Add one for tomorrow

--- google-calendar-interface.js
+++ google-calendar-interface.js
@@ -85,7 +85,7 @@ module.exports = {
       // Delete placeholder sleep event
       const today = new Date();
       let tomorrow = new Date();
-      tomorrow.setDate(today.getDate());
+      tomorrow.setDate(today.getDate() + 1);
 
       calendar.events.list({
         auth: auth,
